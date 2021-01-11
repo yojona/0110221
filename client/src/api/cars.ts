fa8f89582c40459819ee76193fd2d6fa946919ed
Jonathan Ayala <jonathan_ayala@eogresources.com>
2021-01-10 20:49:18 -0700

    update github actions refs

--- client/src/api/cars.ts
+++ client/src/api/cars.ts
@@ -4,9 +4,11 @@ type APIResult = {
   cars: Car[];
 }
 
+const API_BASE = "http://carsapp.azurewebsites.net";
+
 export const fetchCars = async (): Promise<Car[]> => {
   try {
-    const request = await fetch('/api/cars', { mode: 'no-cors'});
+    const request = await fetch(`${API_BASE}/api/cars`);
     const data: APIResult = await request.json();
     return data.cars;
   } catch {
